b28748027ddcbb5dd57bd457173f1c6808d45268
JuanPablo <20243ds139@utez.edu.mx>
2025-07-01 08:00:10 -0600

    avance

--- Integradora/src/main/java/config/DBConnection.java
+++ Integradora/src/main/java/config/DBConnection.java
@@ -7,14 +7,14 @@ import java.sql.DriverManager;
 
     public class DBConnection {
 
-        private static final String URL      = "jdbc:oracle:thin:@SU9QIVWKTPRY2OQI";
+        private static final String URL      = "jdbc:oracle:thin:@su9qivwktpry2oqi_low";
         private static final String USER     = "ADMIN";
         private static final String PASSWORD = "Nanchis77$12";
 
         // Obtiene una conexión nueva
         public static Connection getConnection() throws SQLException {
             // 1. Apunta al directorio donde descomprimiste el wallet
-            System.setProperty("oracle.net.tns_admin", "/Users/nicholassotelo/Desktop/clases/progra/Wallet_SU9QIVWKTPRY2OQI.zip ");
+            System.setProperty("oracle.net.tns_admin", "C:\\Users\\CC10\\Desktop\\nico\\Wallet_SU9QIVWKTPRY2OQI");
             // 2. (Opcional) fuerza la validación de nombre de servidor en el certificado
             System.setProperty("oracle.net.ssl_server_dn_match", "true");
             // 3. Obtiene la conexión usando alias, user y pass
--- Integradora/src/main/java/impl/UsuarioDaoImpl.java
+++ Integradora/src/main/java/impl/UsuarioDaoImpl.java
@@ -8,18 +8,31 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UsuarioDaoImpl implements IUsuarioDao{
-    public boolean login(String correo, String pass) throws Exception{
 
+    @Override 
+    public boolean login(String correo, String pass) throws Exception{
         String sql = "select ID,CORREO,PASS FROM USUARIO WHERE CORREO = ? AND PASS = ?";
         try {
+            Connection con = DBConnection.getConnection();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, correo);
+            ps.setString(2, pass);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                return true;
+            }
+            return false;
+        }catch (Exception e){
+            throw new Exception(e);
+        }
+    }
 
-        }catch (Exception e){}
-        Connection con = DBConnection.getConnection();
-        PreparedStatement ps = con.prepareStatement(sql);
-        ps.setString(1, correo);
-        ps.setString(2, pass);
-        ResultSet rs = ps.executeQuery();
-
-        return false;
+    public static void main (String[] args){
+        UsuarioDaoImmpl dao = new UsuarioDoaImpl();
+        try{
+            System.out.println(dao.login("correo","pass"));
+        }catch (Exception e){
+            throw new RuntimeExcpetion(e);
+        }
     }
 }
